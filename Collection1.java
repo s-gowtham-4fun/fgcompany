e67dce9d4c3498adcb4696d8f3c31b41471e5233
s-gowtham-4fun <gowtham05ap19@gmail.com>
2024-06-24 16:15:05 +0530

    added files

---
+++ Collection1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Collection1 {
+    
+    public static void main(String[] args){
+
+        LinkedList<String> school = new LinkedList<String>();
+        ArrayList<String> names = new ArrayList<String>();
+        Stack <ranks
+
+      }    
+}
---
+++ basic.java
@@ -0,0 +1,28 @@
+ class str {
+    
+    static String str1 = "qwerty";
+
+ void reverse(){
+for( int i = str1.length()-1;i>=0;i--){
+        System.out.print(str1.charAt(i));
+        
+        
+        }
+    }
+    
+}
+
+
+
+
+
+public class basic {
+    public static void main(String[] args){
+            str revword = new str();
+            revword.reverse();
+         
+            
+    }
+    
+    
+}
